31f5788394d2fadae04eb0877714a77e0d5d139f
WolnyKacper <55091973+WolnyKacper@users.noreply.github.com>
2020-02-23 23:23:39 +0100

    Delete add_user.php

--- zsł/11_bazy2/uprawnienia_do_bazy/scripts/add_user.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!empty($_POST['name']) && !empty($_POST['surname']) && !empty($_POST['birthday'])) {
-  $name = $_POST['name'];
-  $surname = $_POST['surname'];
-  $birthday = $_POST['birthday'];
-}else{
-  header('location:../');
-}
- ?>
